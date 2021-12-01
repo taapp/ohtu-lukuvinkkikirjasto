0848684fcd9a755a001177c13b336db5600f1a55
Viivi Baarman <viivi.baarman@gmail.com>
2021-12-01 10:49:22 +0200

    lukuvinkkilista alustettu

--- src/index.py
+++ src/index.py
@@ -1,8 +1,15 @@
 from app import app
 from lukuvinkki import Lukuvinkki
+from lukuvinkkilista import Lukuvinkkilista
 
 
 if __name__ == '__main__':
-    vinkki = Lukuvinkki("kirja", "Kalle Kirjailija", "11111")
-    print(vinkki)
+
+    #testataan, että lukuvinkki ja -lista toimii:
+    vinkkilista = Lukuvinkkilista()
+    vinkkilista.lisaa(Lukuvinkki("kirja", "Kirjan nimi tässä", "Kalle Kirjailija", "11111"))
+    vinkkilista.lisaa(Lukuvinkki("podcast", "Podcastin nimi tässä", "Pertti Podaaja", url="www.osoite.fi", tagit="verkot, lifestyle, sijoittaminen"))
+    vinkkilista.listaa()
+
+    #käynnistää web-sovelluksen localhostille:
     app.run()
