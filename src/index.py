1462fbbb9a45c358ef563f0ce580d6a88402b0f8
Viivi Baarman <viivi.baarman@gmail.com>
2021-12-01 10:43:54 +0200

    alustettu lukuvinkki-olio

--- src/index.py
+++ src/index.py
@@ -1,5 +1,8 @@
 from app import app
+from lukuvinkki import Lukuvinkki
 
 
 if __name__ == '__main__':
+    vinkki = Lukuvinkki("kirja", "Kalle Kirjailija", "11111")
+    print(vinkki)
     app.run()
---
+++ src/lukuvinkki.py
@@ -0,0 +1,18 @@
+class Lukuvinkki:
+    def __init__(self, tyyppi, otsikko, kirjailija, isbn=None, tagit=None, url=None, kommentti=None, kuvaus=None, kurssit=None, luettu=False):
+        self.tyyppi = tyyppi
+        self.otsikko = otsikko
+        self.kirjailija = kirjailija
+        self.isbn = isbn
+        self.tagit = tagit
+        self.url = url
+        self.kommentti = kommentti
+        self.kuvaus = kuvaus
+        self.kurssit = kurssit
+        self.luettu = luettu
+
+    def merkitse_luetuksi(self):
+        self.luettu = True
+
+    def __str__(self):
+        return self.otsikko + ", kirjailija: " + self.kirjailija + ", ISBN: " + str(self.isbn) + ", URL: " + str(self.url) + ", tagit: " + str(self.tagit) + ", kuvaus: " + str(self.kuvaus) + ", kommentit: " + str(self.kommentti) + ", luettu: " + str(self.luettu)
