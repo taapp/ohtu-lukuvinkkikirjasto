4a4557b2b0475e8dd71646df4469f262d0acd465
Viivi Baarman <viivi.baarman@gmail.com>
2021-12-02 12:54:07 +0200

    lukuvinkin lisäys lisätty käyttöliittymään

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
+from entities.lukuvinkki import Lukuvinkki
+from entities.lukuvinkkilista import Lukuvinkkilista
 
 app = Flask(__name__)
 
@@ -8,9 +10,12 @@ def redirect_to_login():
 def redirect_to_register():
     return redirect(url_for("render_register"))
 
+def redirect_to_add_subject():
+    return redirect(url_for("render_add_subject"))
+
 @app.route("/")
 def render_home():
-    return render_template("index.html")
+    return render_template("add_subject.html")
 
 @app.route("/login", methods=["GET"])
 def render_login():
@@ -19,3 +24,34 @@ def render_login():
 @app.route("/register", methods=["GET"])
 def render_register():
     return render_template("register.html")
+
+@app.route("/add_subject", methods=["GET"])
+def render_add_subject():
+    return render_template("add_subject.html")
+
+@app.route("/register", methods=["POST"])
+def handle_register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    password_confirmation = request.form.get("password_confirmation")
+
+@app.route("/add_subject", methods=["POST"])
+def add_subject():
+    tyyppi = request.form.get("tyyppi")
+    otsikko = request.form.get("otsikko")
+    kirjailija = request.form.get("kirjailija")
+    isbn = request.form.get("isbn")
+    tagit = request.form.get("tagit")
+    url = request.form.get("url")
+    kommentti = request.form.get("kommentti")
+    kuvaus = request.form.get("kuvaus")
+    kurssit = request.form.get("kurssit")
+    
+    lukuvinkkilista = Lukuvinkkilista()
+
+    try:
+        lukuvinkkilista.lisaa(Lukuvinkki(tyyppi, otsikko, kirjailija, isbn, tagit, url, kommentti, kuvaus, kurssit))
+        return redirect_to_add_subject()
+    except Exception as error:
+        flash(str(error))
+        return redirect_to_register()
