9844a5e74feb50246a0216a993c7097ce972323d
taapp <julius.nieminen@helsinki.fi>
2021-12-02 08:24:38 +0200

    Korjattu index.py käyttämään vinkki_service-luokkaa

--- src/services/vinkki_service.py
+++ src/services/vinkki_service.py
@@ -5,6 +5,18 @@ from entities.user import User
 class VinkkiService:
     def __init__(self):
         self.users = []
+        self.vinkkilista = Lukuvinkkilista()
 
     def create_user(self, id, username, password):
         return User(id, username, password)
+
+    def create_vinkki(self, tyyppi, otsikko, kirjailija, isbn=None, tagit=None, url=None, kommentti=None, kuvaus=None, kurssit=None, luettu=False):
+        return Lukuvinkki(tyyppi, otsikko, kirjailija, isbn, tagit, url, kommentti, kuvaus, kurssit, luettu)
+
+    def add_vinkki_to_vinkkilista(self, vinkki):
+        self.vinkkilista.lisaa(vinkki)
+
+    def listaa_vinkit(self):
+        self.vinkkilista.listaa()
+
+vinkki_service = VinkkiService()
