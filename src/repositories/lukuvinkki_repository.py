f35f6105f117f7855217dbf1ba659ad0a4b285b4
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2021-12-08 11:12:55 +0200

    lukuvinkkirepository alkuun

---
+++ src/repositories/lukuvinkki_repository.py
@@ -0,0 +1,21 @@
+from entities.lukuvinkki import Lukuvinkki
+from database_connection import get_database_connection
+
+class LukuvinkkiRepository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    
+    def create(self, lukuvinkki):
+        cursor = self._connection.cursor()
+
+        cursor.execute(
+            'insert into lukuvinkit (tyyppi, otsikko, kirjailija, isbn, tagit, url, kommentti, kuvaus, kurssit, luettu) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            (lukuvinkki.tyyppi, lukuvinkki.otsikko, lukuvinkki.kirjailija, lukuvinkki.isbn, lukuvinkki.tagit, lukuvinkki.url, lukuvinkki.kommentti, lukuvinkki.kuvaus, lukuvinkki.kurssit, lukuvinkki.luettu)
+
+        )
+
+        self.connection.commit()
+
+
+lukuvinkki_repository = LukuvinkkiRepository(get_database_connection())
