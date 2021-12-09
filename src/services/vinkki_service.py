ff266cbd5a210f43fa71d53aa26689cec637d404
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2021-12-09 15:37:55 +0200

    pylint-viilausta

--- src/services/vinkki_service.py
+++ src/services/vinkki_service.py
@@ -13,16 +13,20 @@ class VinkkiService:
         self._lukuvinkki_repository = lukuvinkki_repository
 
     def create_user(self, id, username, password):
-        return User(id, username, password)
+        return User(id, username, password) # pylint: disable invalid-name
 
-    def create_vinkki(self, tyyppi, otsikko, kirjailija, isbn=None, tagit=None, url=None, kommentti=None, kuvaus=None, kurssit=None, luettu=False):
-        return Lukuvinkki(tyyppi, otsikko, kirjailija, isbn, tagit, url, kommentti, kuvaus, kurssit, luettu)
+    def create_vinkki(self, tyyppi, otsikko, kirjailija, isbn=None, tagit=None, url=None,
+                        kommentti=None, kuvaus=None, kurssit=None, luettu=False):
+
+        return Lukuvinkki(tyyppi, otsikko, kirjailija, isbn, tagit, url, kommentti,
+                kuvaus, kurssit, luettu)
 
     def add_vinkki_to_vinkkilista(self, vinkki):
         self.vinkkilista.lisaa(vinkki)
 
 #    def listaa_vinkit(self):
 #        self.vinkkilista.listaa()
+
     def listaa_lukuvinkit(self):
         return self._lukuvinkki_repository.hae_vinkit()
 
