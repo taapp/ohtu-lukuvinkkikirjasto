fde5f0391a2273002598f34c8bbea60395047a41
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2021-12-13 12:06:42 +0200

    tarkistusmetodit user serviceen

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -6,13 +6,20 @@ from repositories.user_repository import (
 class UserInputError(Exception):
     pass
 
+class ExistingUserError(Exception):
+    pass
+
 
 class UserService:
     def __init__(self, user_repository=default_user_repository):
         self._user_repository = user_repository
 
-    def create_user(self, username, password):
-#        self.validate(username, password)
+    def create_user(self, username, password, password_confirmation):
+        already_a_user = self._user_repository.find_username(username)
+        if already_a_user:
+            raise ExistingUserError(f"Tunnus on jo käytössä.")
+
+        self.validate(username, password, password_confirmation)
         user = self._user_repository.create(
             User(username, password)
         )
@@ -20,10 +27,10 @@ class UserService:
         return user
 
 
-#    def validate(self, username, password, password_confirmation):
-#        if not username or not password:
-#            raise UserInputError("Käyttäjätunnus tai salasana puuttuu")
-#        if password != password_confirmation:
-#            raise UserInputError("Salasanat eivät täsmää")
+    def validate(self, username, password, password_confirmation):
+        if not username or not password:
+            raise UserInputError("Käyttäjätunnus tai salasana puuttuu")
+        if password != password_confirmation:
+            raise UserInputError("Salasanat eivät täsmää")
 
 user_service = UserService()
