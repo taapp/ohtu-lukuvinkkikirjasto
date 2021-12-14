9eb775f14c20012668f53c930be07ff51bdec511
taapp <julius.nieminen@helsinki.fi>
2021-12-14 08:13:22 +0200

    Lisätty username-kenttä vinkit-tauluun

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -36,7 +36,8 @@ def create_vinkit_table(connection):
             kommentti TEXT,
             kuvaus TEXT,
             kurssit TEXT,
-            luettu INTEGER
+            luettu INTEGER,
+            username TEXT
         );
     ''')
     connection.commit()
@@ -52,8 +53,15 @@ def insert_default_data(connection):
     sql_users = """INSERT INTO users(username, password) VALUES (?,?)"""
     cursor.execute(sql_users, ["tunnus", "passu"])
     cursor.execute(sql_users, ["kayttaja", "salasana"])
-    sql_vinkit = 'insert into vinkit (tyyppi, otsikko, kirjailija, isbn, tagit, url, kommentti, kuvaus, kurssit, luettu) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-    cursor.execute(sql_vinkit, ["kirja", "Sinuhe Egyptiläinen", "Mika Waltari", "11111-22222", "","","Yksi lempikirjoistani!","","",""])
+    sql_vinkit = 'insert into vinkit (tyyppi, otsikko, kirjailija, isbn, tagit, url, kommentti, kuvaus, kurssit, luettu, username) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+    cursor.execute(sql_vinkit, 
+        ["kirja", "Sinuhe Egyptiläinen", "Mika Waltari", "11111-22222", "","","Yksi lempikirjoistani!","","","",None])
+    cursor.execute(sql_vinkit, 
+        ["video", "Merge sort algorithm", "", "", "","https://www.youtube.com/watch?v=TzeBrDU-JaY","Hyvä selitys merge sortin toiminnasta esimerkin avulla","","","","tunnus"])
+    cursor.execute(sql_vinkit, 
+        ["Podcast", "Jim Benson on Personal Kanban, Lean Coffee and collaboration", "Sami Honkonen", "", "Kanban, Lean Coffee","""Personal Kanban, which is an approach to dealing with the overload of stuff you need to deal with. 
+  We dig into into its two simple rules, visualizing work and limiting work in progress. 
+  We then walk through Lean Coffee, which is a simple and effective way to run your meetings.""","","TKT20006 Ohjelmistotuotanto","","","kayttaja"])
     connection.commit()
 
 
