2dd363840b2e5e943896509f25ee5ffa52ff3272
taapp <julius.nieminen@helsinki.fi>
2021-12-16 07:10:11 +0200

    Poistettu turhia rivejä app.py:stä

--- src/index.py
+++ src/index.py
@@ -1,22 +1,4 @@
 from app import app
-from database_connection import get_database_connection
-from services.vinkki_service import vinkki_service
-
-def load_user(connection, username, password):
-    cursor = connection.cursor()
-    cursor.execute("SELECT id,name,password FROM users WHERE name=? AND password=?", [
-                    username, password])
-    res = cursor.fetchone()
-    if res is not None:
-        user = User(res[0], res[1], res[2])
-        return user
-    return None
-
-def main():
-    print("Hello world!")
-    connection = get_database_connection()
-    user = load_user(connection, 'user', 'passu')
-    print(user)
 
 if __name__ == '__main__':
     #käynnistää web-sovelluksen localhostille:
